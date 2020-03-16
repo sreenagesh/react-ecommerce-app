217990cded466f1df1f620bc2485a101043a858f
shyam888@ <sreenageshu@gmail.com>
2020-03-15 20:26:52 -0400

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import HomePage from './homepage.component';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
 
-import Header from './components/header/header';
-import HomePage from './pages/homepage/homepage';
-import TiendaPage from './pages/tienda/tienda';
+function App() {
 
-class App extends Component {
-  render() {
-    return <div className='App'>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/tienda/' component={TiendaPage} />
-      </Switch>
-    </div>;
-  }
+  return (<div> <HomePage /></div>);
+
 }
 
 export default App;
